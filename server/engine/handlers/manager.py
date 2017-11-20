b66abfc8885a85b4ee738a66f96f8920ce43009c
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2017-11-20 00:10:31 +0000

    update manager logic to work with redis and persist learner state across multiple next move calls

--- server/engine/handlers/manager.py
+++ server/engine/handlers/manager.py
@@ -1,7 +1,7 @@
 import os, sys, argparse, pdb
 sys.path.insert(0, os.environ['DQN_ROOT'])
 
-import numpy as np
+import numpy as np, redis
 
 from learner.learning_engine import LearningEngine
 
@@ -16,25 +16,41 @@ def generate_obstacles(x_max, y_max, obstacles_lim):
     obstacles = set([ (i, j) for i, j in zip(obstacle_x, obstacle_y) ])
     return list(obstacles)
 
-def next_move(learner, user_action, turn, retry_limit=5):
+def next_move(user_key, retry_limit=5):
     action_ls = ['up', 'down', 'left', 'right']
-    print('\nUpdating game state by user action..')
-    state, reward, done, _ = learner.env.step(action_ls.index(user_action), turn)
     
-    print('\nUpdating game state by agent action...')
-    agent_action = learner.agent.select_action(state)
-    next_state, _, done, _ = learner.env.step(agent_action[0, 0], (turn + 1) % 2)
-    retries = 0
-    pdb.set_trace()
-    while np.array_equal(state.cpu().numpy(), next_state.cpu().numpy()):
-        print('\nUpdating game state by agent action (retry {})...'.format(retries))
-        agent_action = learner.agent.select_action(state, retries < retry_limit)
-        next_state, _, done, _ = learner.env.step(agent_action[0, 0], (turn + 1) % 2)
-        retries += 1
+    #  initialize connection to redis
+    r = redis.Redis(host='redis')
 
-    return action_ls[agent_action[0, 0]]
+    #  get the learner object from the user's store
+    init_user_state = r.get(user_key)
+    learner = init_user_state['learning_engine']
 
-def init_learning_engine():
+    while True:
+        #  get updated action and turn from user store
+        curr_user_state = r.get(user_key)
+        user_action, turn = curr_user_state['action'], curr_user_state['turn']
+
+        #  get the current state of the game post user action
+        print('\nUpdating game state by user action..')
+        state, _, _, _ = learner.env.step(action_ls.index(user_action), turn)
+        
+        #  make agent choose an action; repeat if chosen action is invalid
+        print('\nUpdating game state by agent action...')
+        agent_action = learner.agent.select_action(state)
+        next_state, _, _, _ = learner.env.step(agent_action[0, 0], (turn + 1) % 2)
+        retries = 0
+        while np.array_equal(state.cpu().numpy(), next_state.cpu().numpy()):
+            print('\nUpdating game state by agent action (retry {})...'.format(retries))
+            #  agent chooses randomly if too many invalid moves are chosen
+            #  otherwise choose from learned weights
+            agent_action = learner.agent.select_action(state, retries < retry_limit)
+            next_state, _, done, _ = learner.env.step(agent_action[0, 0], (turn + 1) % 2)
+            retries += 1
+
+        yield action_ls[agent_action[0, 0]]
+
+def init_learning_engine(user_key):
     print('\nInitializing learning engine. Please wait..')
     learner = LearningEngine(os.environ['MODEL_NAME'])
 
@@ -43,11 +59,19 @@ def init_learning_engine():
     obstacles = generate_obstacles(game_width-1, game_height-1, 
                                 ( game_width * game_height ) // 2)
     
+    player_pos = [ (game_height - 1, 0), (0, game_width - 1) ]
     learner.init_game(game_width, game_height, obstacles)
 
     print('Learning engine initialization complete!\n')
-    return learner
-    
+
+    game_meta = { 'grid_height': game_height, 'grid_width': game_width, \
+                    'player_pos': player_pos ,  'obstacles': obstacles }
+    r = redis.Redis(host='redis')
+    user_state = r.get(user_key)
+    user_state['game_meta'] = game_meta
+    user_state['learning_engine'] = learner
+    return True
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
