415514140f48c5d6cc0bfc43cf0d1f1f9673fe36
Abhimanyu Banerjee <banerjee.abhimanyu3@gmail.com>
2018-03-23 00:08:12 +0000

    fix: pre-game events and listeners

--- server/src/namespaces/player_v_player.py
+++ server/src/namespaces/player_v_player.py
@@ -2,17 +2,14 @@ import socketio
 
 num_players = 0
 game_meta = {
-    'decision_meta': {
-        'paddle_vel_incr': 8,
-        'fps': 60,
-        'player_idx': 0,
-        'rendering_meta': {  
-            'screen_width': 600, 
-            'screen_height': 400,
-            'ball_radius': 20,
-            'pad_width': 8,
-            'pad_height': 80
-        }
+    'paddle_vel_incr': 8,
+    'fps': 6,
+    'rendering_meta': {  
+        'screen_width': 600, 
+        'screen_height': 400,
+        'ball_radius': 20,
+        'pad_width': 8,
+        'pad_height': 80
     }
 }
 
@@ -27,15 +24,17 @@ class PVPNamespace(socketio.AsyncNamespace):
         global num_players
 
         print('\n{} is requesting to play'.format(sid))
-        num_players += 1
-        print('\n{} users requesting to play'.format(num_players))
+        game_meta['player_idx'] = num_players % 2 
+        print('Game meta for player {} is {}'.format(game_meta['player_idx'], game_meta))
 
-        await self.emit('ws_game_init_resp', game_meta, skip_sid=sid) 
+        num_players += 1
+
+        await self.emit('ws_game_init_resp', {'game_meta': game_meta}, room=sid) 
 
         if num_players >= 2:
             print('Ready players One!')
             await self.emit('ws_game_start')
 
     async def on_c_keypress_propagate(self, sid, environ):
-        print("{} pressed key {}".format(sid, environ['key']))
-        await self.emit('ws_keypress_notify', environ, skip_sid=sid)
+        print('Received a keypress: {} notif from {}'.format(environ, sid))
+        await self.emit('ws_keypress_notify')
